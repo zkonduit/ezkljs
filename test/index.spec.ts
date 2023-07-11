8628f28eaa181735f2ee9196f6f4a7816b402aec
Ethan Cemer <tylercemer@gmail.com>
2023-07-11 18:56:59 -0500

    *added simulateVerify

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,28 +1,65 @@
-import { parseProof } from '../src';
+import { parseProof, simulateVerify } from '../src';
 import { ethers } from 'hardhat';
 import { Verifier } from '../test/typechain-types/Verifier';
 import { assert } from 'ethers';
 
 describe('ezkl', () => {
+  let Verifier: Verifier;
+  let proofPath: string = "./test/data/test.pf"
+  let rpc_url: string = "http://127.0.0.1:8545/"
+  beforeAll(async () => {
+    // Instantiate contract
+    Verifier = await ethers.deployContract("Verifier");
+  });
   describe('parseProof', () => {
     it('should return pubInputs and proof from test.pf file', async() => {
 
-      let proofPath = "./test/data/test.pf"
-
       let [pubInputs, proof] = parseProof(proofPath);
-    
-      // Instantiate contract
-      let Verifier: Verifier
-  
-      Verifier = await ethers.deployContract("Verifier")
-  
+
       console.info("publicInputs: ", pubInputs);
       console.info("proof: ", proof);
   
       // Call verify function and return results
       const result = await Verifier.verify(pubInputs, proof);
 
-      assert(result == true, "Parsing worked", "BAD_DATA");
+      assert(result == true, "Proof parsing worked", "BAD_DATA");
     });
   });
+  describe('simulateVerify', () => {
+    it('should return true for test.pf file', async() => {
+      let address = await Verifier.getAddress();
+      // get provider
+      let provider = ethers.provider;
+      let abi = [
+        {
+          "inputs": [
+            {
+              "internalType": "uint256[]",
+              "name": "pubInputs",
+              "type": "uint256[]"
+            },
+            {
+              "internalType": "bytes",
+              "name": "proof",
+              "type": "bytes"
+            }
+          ],
+          "name": "verify",
+          "outputs": [
+            {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ]
+      console.log(proofPath)
+      let [pubInputs, proof] = parseProof(proofPath);
+      let result = await simulateVerify(pubInputs, proof, provider, address, abi);
+      assert(result == true, "Simulate verify worked", "BAD_DATA");
+    });
+  })
 });
