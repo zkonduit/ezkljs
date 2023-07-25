e9e5f13cdda791981e58eb263e6f9cd89ac1c1e9
Casey <leptone55@gmail.com>
2023-07-25 22:08:07 +0100

    fix: remove everything that isn't being used

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# ezkl
-
-## 1.0.0
-
-### Major Changes
-
-- Added proof parser and simulate verify helper functions.
---
+++ src/index.d.ts
@@ -0,0 +1,36 @@
+export interface Artifact {
+  name: string
+  description: string
+  id: string
+}
+export interface ProveResponse {
+  prove: {
+    taskId: string
+    status: string
+  }
+}
+export interface Witness {
+  inputs: number[][]
+  outputs: number[][]
+  maxLookupInputs: number
+}
+export interface ProofDetails {
+  taskId: string
+  status: 'SUCCESS' | 'PENDING' | 'FAILED'
+  proof: string
+  witness: Witness
+}
+
+declare function artifacts(): Promise<Artifact[]>
+declare function prove(id: string, input: Buffer): Promise<ProveResponse>
+declare function getProof(taskId: string): Promise<ProofDetails>
+
+declare const Router: {
+  artifacts: typeof artifacts
+  prove: typeof prove
+  getProof: typeof getProof
+}
+
+import Wasm from './wasm'
+
+export { Router, Wasm }
