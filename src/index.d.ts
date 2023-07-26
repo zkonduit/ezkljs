e24c2c2dc693f40fdaa42199926c3d396264cab2
Casey <leptone55@gmail.com>
2023-07-26 02:18:02 +0100

    fix: restructure so ezkljs works with module resolution algorithm

--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ethers } from 'ethers';
-export declare function parseProof(proofFilePath: string): [bigint[], string];
-export declare function simulateVerify(pubInputs: bigint[], proof: string, provider: ethers.Provider, contractAddress: string, abi: ethers.InterfaceAbi): Promise<boolean>;
