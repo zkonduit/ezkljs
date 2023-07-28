3c75362c596103819b71aa38dc01a1cc15f76019
Casey <leptone55@gmail.com>
2023-07-28 03:13:39 +0100

    createUploadLink

--- src/router.ts
+++ src/router.ts
@@ -6,41 +6,65 @@
 //   ApolloProvider,
 //   gql,
 // } from '@apollo/client/core';
+import fs from 'fs';
+import path from 'path';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
+import { createUploadLink } from 'apollo-upload-client';
 
 const URL = 'http://127.0.0.1:5000/graphql';
 
 const client = new ApolloClient({
-  uri: URL,
+  // uri: URL,
   cache: new InMemoryCache(),
+  link: createUploadLink({ uri: URL }),
 });
-
-interface Witness {
-  inputs: string[];
-  outputs: string[];
-  maxLookupInputs: number;
-}
-
-interface Proof {
-  taskId: string;
-  status: string;
-  proof: string;
-  witness: Witness;
-}
-
-interface ProofQueryResult {
-  proof: Proof;
-}
 
 function isUUID(uuid: string): boolean {
   const uuidRegex =
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
   return uuidRegex.test(uuid);
 }
-interface ProveMutationResult {
-  prove: Proof;
+
+interface Upload {
+  stream: fs.ReadStream;
+  filename: string;
+  mimetype: string;
 }
 
+async function prove(artifactId: string, input: Upload) {
+  if (!isUUID(artifactId)) {
+    throw new Error('Invalid UUID');
+  }
+
+  const { data } = await client.mutate<ProveMutationResult>({
+    mutation: gql`
+      mutation Prove($id: String!, $input: Upload!) {
+        prove(id: $id, input: $input) {
+          taskId
+          status
+        }
+      }
+    `,
+    variables: {
+      id: artifactId,
+      input,
+    },
+  });
+
+  console.log(data);
+  // return data.prove;
+}
+
+const inputFilePath = path.resolve(__dirname, 'public', 'input.json');
+const inputStream = fs.createReadStream(inputFilePath);
+
+const uploadInput: Upload = {
+  stream: inputStream,
+  filename: path.basename(inputFilePath),
+  mimetype: 'application/json', // Or whatever mimetype is appropriate
+};
+
+void prove('4cd02d5f-3499-4c4a-8e82-e0e8c7c367bd', uploadInput);
 // type UUID = typeof v4;
 // async function prove(artifactId: string, input: File) {
 //   if (!isUUID(artifactId)) {
@@ -95,9 +119,9 @@ async function afrifacts() {
     query: GET_ARTIFACTS,
   });
 
+  // console.log(artifacts);
+
   return artifacts;
 }
-
-void afrifacts();
 
 export default { afrifacts };
