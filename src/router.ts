48d85ece06a5f94910f0c8fc71a829f4a7c75dc0
Casey <leptone55@gmail.com>
2023-07-28 02:14:40 +0100

    apollo client

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,19 @@
 // import { Artifact } from 'hardhat/types';
 // import request from './utils/request';
-import { request, gql } from 'graphql-request';
-
-const URL = 'http://127.0.0.1:5000/graphql';
 // import {
 //   ApolloClient,
 //   InMemoryCache,
 //   ApolloProvider,
 //   gql,
-// } from '@apollo/client';
+// } from '@apollo/client/core';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
 
-// import input from './input.json';
+const URL = 'http://127.0.0.1:5000/graphql';
 
-// const client = new ApolloClient({
-//   uri: 'http://127.0.0.1:5000/graphql',
-//   cache: new InMemoryCache(),
-// });
-// GET
-// healthCheck
-async function healthCheck() {
-  const { data } = await client.query({
-    query: gql`
-      query {
-        check
-      }
-    `,
-  });
-  return data.check;
-}
+const client = new ApolloClient({
+  uri: URL,
+  cache: new InMemoryCache(),
+});
 
 interface Witness {
   inputs: string[];
@@ -46,27 +32,6 @@ interface ProofQueryResult {
   proof: Proof;
 }
 
-// // getProof
-// async function getProof() {
-//   const { data } = await client.query<ProofQueryResult>({
-//     query: gql`
-//       query {
-//         proof(taskId: "506e2d28-b9e5-4135-986a-405a34ee8eab") {
-//           taskId
-//           status
-//           proof
-//           witness {
-//             inputs
-//             outputs
-//             maxLookupInputs
-//           }
-//         }
-//       }
-//     `,
-//   });
-//   return data.proof;
-// }
-
 function isUUID(uuid: string): boolean {
   const uuidRegex =
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
@@ -77,11 +42,11 @@ interface ProveMutationResult {
 }
 
 // type UUID = typeof v4;
-async function prove(artifactId: string, input: File) {
-  if (!isUUID(artifactId)) {
-    throw new Error('Invalid UUID');
-  }
-}
+// async function prove(artifactId: string, input: File) {
+//   if (!isUUID(artifactId)) {
+//     throw new Error('Invalid UUID');
+//   }
+// }
 
 //   const { data } = await client.mutate<ProveMutationResult>({
 //     mutation: gql`
@@ -115,7 +80,7 @@ type ArtifactsResponse = {
 };
 async function afrifacts() {
   const GET_ARTIFACTS = gql`
-    {
+    query GetArtifacts {
       artifacts {
         name
         description
@@ -123,11 +88,16 @@ async function afrifacts() {
       }
     }
   `;
-  const { artifacts } = await request<ArtifactsResponse>(URL, GET_ARTIFACTS);
+
+  const {
+    data: { artifacts },
+  } = await client.query<ArtifactsResponse>({
+    query: GET_ARTIFACTS,
+  });
 
   return artifacts;
 }
 
 void afrifacts();
 
-export default { healthCheck };
+export default { afrifacts };
