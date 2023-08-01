17adc2b235a32c1a22ce8eab1bbf141edd6424f8
Casey <leptone55@gmail.com>
2023-08-01 22:20:45 +0100

    add: use prettier to keep style consistent

--- src/utils/stringValidators.ts
+++ src/utils/stringValidators.ts
@@ -4,6 +4,6 @@ export function isValidHexString(test: string) {
 
 export function isValidV4UUID(test: string) {
   return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
-    test
+    test,
   )
 }
