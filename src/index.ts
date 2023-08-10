279a87a2d209f94a8a46b898c6b541489179df3c
Casey <leptone55@gmail.com>
2023-08-10 02:05:29 +0100

    cp in engine

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import { router } from './submodules/router'
+// import { Helper } from './submodules/helper'
 
-export { router }
+import init, {
+  poseidonHash,
+  elgamalEncrypt,
+  elgamalDecrypt,
+  genPk,
+  genVk,
+  verify,
+  prove,
+} from './submodules/engine/ezkl'
+
+const engine = {
+  init,
+  poseidonHash,
+  elgamalEncrypt,
+  elgamalDecrypt,
+  genPk,
+  genVk,
+  verify,
+  prove,
+}
+
+export { router, engine }
