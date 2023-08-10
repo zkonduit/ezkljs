2136370fd02fa9f800cbb3a00e6ea94faefa9494
Casey <leptone55@gmail.com>
2023-08-10 20:57:03 +0100

    postBundle

---
+++ postBundle.cjs
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+const bundlePath = path.resolve(__dirname, 'dist/bundle.commonjs.js')
+
+fs.readFile(bundlePath, 'utf8', (err, content) => {
+  if (err) {
+    console.error('An error occurred:', err)
+    return
+  }
+
+  const modifiedContent = content.replace(
+    'module.exports=r',
+    'module.exports=r.default',
+  )
+
+  fs.writeFile(bundlePath, modifiedContent, 'utf8', (writeErr) => {
+    if (writeErr) {
+      console.error('An error occurred while writing the file:', writeErr)
+    } else {
+      console.log('File successfully modified.')
+    }
+  })
+})
