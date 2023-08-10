1f1f0dd542c9230975b523445cd73269dd840405
Casey <leptone55@gmail.com>
2023-08-10 19:25:09 +0100

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-import { router } from './submodules/router'
+import { router } from './router'
 
-async function loadEngine() {
-  if (typeof window !== 'undefined') {
-    const module = await import('./submodules/engine/ezkl')
-    const { default: init, poseidonHash, genPk, genVk, verify, prove } = module
-    const engine = {
-      init,
-      poseidonHash,
-      genPk,
-      genVk,
-      verify,
-      prove,
-    }
-    return engine
-  } else {
-    console.log('Node.js')
-    return null
-  }
-}
-// export { router, loadEngine }
-// export default { router, loadEngine }
+export { router, loadEngine }
+export default { router, loadEngine }
 
-export { router }
-export default { router }
+// export { router }
+// export default { router }
