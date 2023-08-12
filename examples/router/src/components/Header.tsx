d7c6dd4ba48dd59b79595d741103da27e9dc244b
Casey <leptone55@gmail.com>
2023-08-12 16:54:10 +0100

    sts

--- examples/router/src/components/Header.tsx
+++ examples/router/src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   // const { open, setOpen } = useAPINavColumnStore()
   const { open, setOpen } = useAPINavColumnStore()
   return (
-    <div className='bg-white fixed w-full top-0 left-0 z-50 h-16  border-b-2 border-slate-300 flex items-center justify-between'>
+    <div className='bg-white fixed w-full top-0 left-0 z-50 h-16  border-b-2 border-slate-300  lg:px-10 md:px-6 sm:px-4 px-2  flex items-center justify-between'>
       {/* left */}
       <div className='w-full md:w-2/12 flex'>
         {!open ? (
