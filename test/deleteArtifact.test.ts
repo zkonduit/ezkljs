e0b9d638043897b89f75521eea1c07c7d683e16e
Casey <leptone55@gmail.com>
2023-12-01 10:28:33 -0800

    tests for delete

--- test/deleteArtifact.test.ts
+++ test/deleteArtifact.test.ts
@@ -3,13 +3,14 @@ import hub from '../src'
 import { GQL_URL, ORG_ID } from '../src/utils/constants'
 
 import fs from 'node:fs'
+import exp from 'node:constants'
 
 describe('delete artifact', () => {
   let id: string | undefined
 
   const artifactName = 'test delete artifact 4'
   // const organizationName = 'currenthandle'
-  beforeEach(async () => {
+  beforeAll(async () => {
     const modelFile = fs.readFileSync(
       path.resolve(__dirname, 'proof_artifacts', 'network.onnx'),
     )
@@ -40,20 +41,29 @@ describe('delete artifact', () => {
   })
 
   it('deletes an artifact', async () => {
-    // if (!artifact) {
-    //   throw new Error('artifact not found')
-    // }
-
     const deletedArtifact = await hub.deleteArtifact({
       name: artifactName,
       organizationName: 'currenthandle',
       url: GQL_URL,
     })
 
-    console.log(deletedArtifact)
-    console.log('test')
+    expect(deletedArtifact).toBeDefined()
+    expect(deletedArtifact).toEqual(id)
+  })
 
-    // expect(deletedArtifact).toBeDefined()
-    // expect(deletedArtifact?.id).toEqual(artifact.id)
+  it("throws an error if the artifact does't exist", async () => {
+    try {
+      const deletedArtifact = await hub.deleteArtifact({
+        name: 'not a real artifact',
+        organizationName: 'currenthandle',
+        url: GQL_URL,
+      })
+
+      console.log('asdsda', deletedArtifact)
+    } catch (e) {
+      expect(e.message).toEqual(
+        'No artifacts found matching the provided criteria.',
+      )
+    }
   })
 })
