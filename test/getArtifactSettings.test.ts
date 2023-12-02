d11fdb675191598c10a5f4286d0652478d148e75
Casey <leptone55@gmail.com>
2023-12-01 18:46:09 -0800

    settings

--- test/getArtifactSettings.test.ts
+++ test/getArtifactSettings.test.ts
@@ -1,5 +1,9 @@
 // import hub from '../dist/bundle.cjs'
+import fs from 'node:fs'
 import hub from '../src'
+import { setTimeout } from 'node:timers/promises'
+import path from 'node:path'
+import { GQL_URL, ORG_ID } from '../src/utils/constants'
 
 const baseUrl = 'https://hub-staging.ezkl.xyz' as const
 
@@ -7,16 +11,78 @@ interface ArtifactSettingsResponse {
   num_rows: number
 }
 
+const artifactName = 'test Artifact settings'
+
 describe('get artifacts settings', () => {
-  it('gets an artifact by id', async () => {
-    const settingsResp = await hub.getArtifactSettings({
-      url: baseUrl,
-      id: '3f47f257-601a-4ba8-8216-e3aa4959fac7',
+  let id: string | undefined
+
+  beforeAll(async () => {
+    const modelFile = fs.readFileSync(
+      path.resolve(__dirname, 'proof_artifacts', 'network.onnx'),
+    )
+    const inputFile = fs.readFileSync(
+      path.resolve(__dirname, 'proof_artifacts', 'input.json'),
+    )
+
+    if (!modelFile) {
+      throw new Error('modelFile not found')
+    }
+
+    if (!inputFile) {
+      throw new Error('inputFile not found')
+    }
+
+    id = await hub.genArtifact({
+      description: 'test delete artifact',
+      name: artifactName,
+      organizationId: ORG_ID,
+      uncompiledModelFile: modelFile,
+      inputFile,
+      url: GQL_URL,
     })
 
-    expect(settingsResp).toBeDefined()
+    if (!id) {
+      throw new Error('id not found')
+    }
 
-    const typedSettingsResp = settingsResp as ArtifactSettingsResponse
-    expect(typedSettingsResp['num_rows']).toBeGreaterThan(0)
+    await setTimeout(5_000)
+  }, 7_0000)
+
+  it('gets an artifact by id', async () => {
+    if (!id) {
+      throw new Error('id not found')
+    }
+    try {
+      console.log('id', id)
+      const settingsResp = await hub.getArtifactSettings({
+        url: baseUrl,
+        id,
+      })
+
+      expect(settingsResp).toBeDefined()
+      console.log('settingsResp!!!', settingsResp)
+
+      const typedSettingsResp = settingsResp as ArtifactSettingsResponse
+      console.log('typedSettingsResp', typedSettingsResp)
+    } catch (e) {
+      console.log('error', e)
+    }
+  }, 10_000)
+
+  afterAll(async () => {
+    if (!id) {
+      throw new Error('id not found')
+    }
+    try {
+      const deletedArtifact = await hub.deleteArtifact({
+        name: artifactName,
+        organizationName: 'currenthandle',
+        url: GQL_URL,
+      })
+      expect(deletedArtifact).toBeDefined()
+      expect(deletedArtifact).toEqual(id)
+    } catch (e) {
+      console.log('error', e)
+    }
   })
 })
